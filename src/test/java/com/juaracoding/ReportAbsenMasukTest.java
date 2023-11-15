26a8cf363f67adc78d4a221535c27f35e4a18342
gugunwisetyo <gugunwisetyo@gmail.com>
2023-11-15 16:19:59 +0700

    manual merge branch aldi and usep v2

--- src/test/java/com/juaracoding/ReportAbsenMasukTest.java
+++ src/test/java/com/juaracoding/ReportAbsenMasukTest.java
@@ -1,7 +1,7 @@
 package com.juaracoding;
 
 import com.juaracoding.drivers.DriverSingleton;
-import com.juaracoding.pages.AdminReportAbsenMasukPage;
+import com.juaracoding.pages.ReportAbsenMasukPage;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import io.cucumber.java.en.And;
@@ -14,7 +14,7 @@ import org.testng.Assert;
 public class ReportAbsenMasukTest {
     private static WebDriver driver;
     static ExtentTest extentTest;
-    private static AdminReportAbsenMasukPage adminReportAbsenMasukPage = new AdminReportAbsenMasukPage();
+    private static ReportAbsenMasukPage reportAbsenMasukPage = new ReportAbsenMasukPage();
 
     public String flagDate;
 
@@ -25,76 +25,76 @@ public class ReportAbsenMasukTest {
 
     @Given("user login as admin")
     public void user_login_as_admin(){
-        adminReportAbsenMasukPage.setUsername("admin_tms");
-        adminReportAbsenMasukPage.setPassword("d1k4@passw0rd");
+        reportAbsenMasukPage.setUsername("admin_tms");
+        reportAbsenMasukPage.setPassword("d1k4@passw0rd");
         DriverSingleton.delay(3);
-        adminReportAbsenMasukPage.setLoginBtn();
+        reportAbsenMasukPage.setLoginBtn();
         extentTest.log(LogStatus.PASS, "user login as staff");
     }
     @And("user click privilege report absen masuk")
     public void user_click_privilege_report_absen_masuk(){
-        adminReportAbsenMasukPage.setPrivilegeReportAbsen();
+        reportAbsenMasukPage.setPrivilegeReportAbsen();
         DriverSingleton.delay(1);
         extentTest.log(LogStatus.PASS, "user click privilege menu");
     }
 
     @And("user click menu report absen masuk")
     public void user_click_menu_report_absen_masuk(){
-        adminReportAbsenMasukPage.setMenuReportAbsenMasuk();
+        reportAbsenMasukPage.setMenuReportAbsenMasuk();
         extentTest.log(LogStatus.PASS, "user click report absen masuk");
     }
 
     @Then("user get text header report absen masuk")
     public void user_get_text_header_report_absen_masuk(){
         DriverSingleton.delay(3);
-        Assert.assertEquals(adminReportAbsenMasukPage.getHeader(),"Report Absen Masuk");
+        Assert.assertEquals(reportAbsenMasukPage.getHeader(),"Report Absen Masuk");
         extentTest.log(LogStatus.PASS, "user get text header");
     }
 
     @And("user input valid position masuk")
     public void user_input_valid_position_masuk(){
-        adminReportAbsenMasukPage.setPosition("ALL");
+        reportAbsenMasukPage.setPosition("ALL");
         extentTest.log(LogStatus.PASS, "user input valid position");
     }
     @And("user input valid branch masuk")
     public void user_input_valid_branch_masuk(){
-        adminReportAbsenMasukPage.setBranch("ALL");
+        reportAbsenMasukPage.setBranch("ALL");
         extentTest.log(LogStatus.PASS, "user input valid branch");
     }
 
     @And("user input valid unit masuk")
     public void user_input_valid_unit_masuk(){
-        adminReportAbsenMasukPage.setUnit("ALL");
+        reportAbsenMasukPage.setUnit("ALL");
         extentTest.log(LogStatus.PASS, "user input valid unit");
     }
 
     @And("user input valid start date masuk")
     public void user_input_valid_start_date_masuk(){
-        adminReportAbsenMasukPage.setStartDate("2023-10-31");
+        reportAbsenMasukPage.setStartDate("2023-10-31");
         flagDate = "2023-10-31";
         extentTest.log(LogStatus.PASS, "user input valid start date");
     }
     @And("user input invalid start date masuk")
     public void user_input_invalid_start_date_masuk(){
-        adminReportAbsenMasukPage.setStartDate("");
+        reportAbsenMasukPage.setStartDate("");
         flagDate = "";
         extentTest.log(LogStatus.PASS, "user input empty start date");
     }
 
     @And("user input valid end date masuk")
     public void user_input_valid_end_date_masuk(){
-        adminReportAbsenMasukPage.setEndDate("2023-11-14");
+        reportAbsenMasukPage.setEndDate("2023-11-14");
         extentTest.log(LogStatus.PASS, "user input valid end date");
     }
     @And("user click export data absen masuk")
     public void user_click_export_data_absen_masuk(){
-        adminReportAbsenMasukPage.setExportBtn();
+        reportAbsenMasukPage.setExportBtn();
         extentTest.log(LogStatus.PASS, "user click export button");
     }
 
     @And("user input invalid end date masuk")
     public void user_input_invalid_end_date_masuk(){
-        adminReportAbsenMasukPage.setStartDate("");
+        reportAbsenMasukPage.setStartDate("");
         extentTest.log(LogStatus.PASS, "user input empty end date");
     }
 
@@ -174,18 +174,18 @@ public class ReportAbsenMasukTest {
 
     @And("user click profile button report masuk")
     public void user_click_profile_button_report_masuk(){
-        adminReportAbsenMasukPage.setProfile();
+        reportAbsenMasukPage.setProfile();
         extentTest.log(LogStatus.PASS, "user click profile button");
     }
 
     @Then("user click logout button report masuk")
     public void user_click_logout_button_report_masuk(){
-        adminReportAbsenMasukPage.setLogoutBtn();
+        reportAbsenMasukPage.setLogoutBtn();
         extentTest.log(LogStatus.PASS, "user click logout button");
     }
     @Then("user get data table report masuk")
     public void user_get_data_table_report_masuk(){
-        if(adminReportAbsenMasukPage.getDataTable().equals("dataTables_empty")){
+        if(reportAbsenMasukPage.getDataTable().equals("dataTables_empty")){
             extentTest.log(LogStatus.FAIL, "table is empty");
         } else {
             extentTest.log(LogStatus.PASS, "user get data on table");
